2f80365f4b05052209f242a3a67c38bcea6af0e5
Pravesh Gupta <praveshgupta1993@gmail.com>
2020-03-15 01:54:07 -0400

    unfinished changes

--- concordia/demos/graph.pl
+++ concordia/demos/graph.pl
@@ -14,3 +14,25 @@ edge(i,k).
 path(N1, N2) :- edge(N1, N2).
 path(N1, N2) :- edge(N1, N), path(N, N2).
 is-connected(N1, N2) :- path(N1, N2).
+
+isdirectlyconnected(N1, N2) :- write('isdirectlyconnected'), nl, edge(N1, N2).
+
+add(X, L, [X|L]).
+n(N1, N2, L) :- write('n-1: ')
+                    , write(N1)
+                    , write(' ')
+                    , write(N2)
+                    , nl
+                    , edge(N1, N2)
+                    , L is [N2| L]
+                    , L is [N1| L].
+
+n(N1, N2, L) :- write('n-1: ')
+                    , write(N1)
+                    , write(' ')
+                    , write(N2)
+                    , write(' ')
+                    , write(L)
+                    , nl
+                    , edge(N1, N)
+                    , n(N, N2, L).
